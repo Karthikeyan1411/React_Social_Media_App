4ec4d727f6d8430f4b30c42c55afff4bc53b8e3d
Karthikeyan <karthikeyan1411m@gmail.com>
2024-09-16 14:51:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import NewPost from './components/NewPost';
+import PostPage from './components/PostPage';
+import About from './components/About';
+import Missing from './components/Missing';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import EditPost from './components/EditPost';
+import { DataProvider } from './context/DataContext';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+        <Header 
+          title='Social Media' 
+        />
+        <Nav />
+        <Routes>
+          <Route path="/" element={ 
+            <Home />
+          }
+          />
+          <Route path="post">
+            <Route index element={<NewPost />} />
+            <Route path=":id" element={<PostPage />} />
+          </Route>
+          <Route path='/edit/:id' element={<EditPost/>}/>
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </div>
   );
 }
